1c3cf621eb27702bdd20364e44a3a79364f03731
omerkaan0x <omerkaan6503@gmail.com>
2024-08-17 00:49:00 +0300

    resource

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class ResourceController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public enum ResourceType { Wood, Stone, Gold }
+    private Dictionary<ResourceType, int> resources = new Dictionary<ResourceType, int>();
+
     void Start()
     {
-        
+
+        foreach (ResourceType type in System.Enum.GetValues(typeof(ResourceType)))
+        {
+            resources[type] = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddResource(ResourceType type, int amount)
     {
-        
+        resources[type] += amount;
+    }
+
+    public bool RemoveResource(ResourceType type, int amount)
+    {
+        if (resources[type] >= amount)
+        {
+            resources[type] -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int GetResourceAmount(ResourceType type)
+    {
+        return resources[type];
     }
 }
