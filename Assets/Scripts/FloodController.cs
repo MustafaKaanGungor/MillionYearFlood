51111a1565563bad27b9142c753c6f4c591ca689
Ahmet Buğra Erbaş <ahmetbgr2@gmail.com>
2024-08-17 00:29:01 +0300

    Added City Controller

--- Assets/Scripts/FloodController.cs
+++ Assets/Scripts/FloodController.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CityController : MonoBehaviour
+public class FloodController : MonoBehaviour
 {
-    public float speed = 1f;
-    public bool isWaiting = false;
-
+    private float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,22 +15,8 @@ public class CityController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isWaiting) return;
-
-
         MoveUp();
-        
-
     }
-
-    public void ToggleWait() {
-        isWaiting = !isWaiting;
-    }
-
-    public void Wait() {
-
-    }
-
 
     public void MoveUp() {
         transform.position += Vector3.up * Time.deltaTime * speed;
